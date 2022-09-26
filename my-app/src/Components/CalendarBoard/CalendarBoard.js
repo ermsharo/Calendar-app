d5680a760fcb9fd20fc1eb4205a5b7b936e4c15c
emilio <ermsharo@gmail.com>
2022-09-26 20:00:29 -0300

    update our changes

--- my-app/src/Components/CalendarBoard/CalendarBoard.js
+++ my-app/src/Components/CalendarBoard/CalendarBoard.js
@@ -1,64 +1,165 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const Board = styled.div`
-display: grid;
-grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-width: 60vw;
-padding: 32px;
-margin: auto;
-background-color: #F6F9FA;
-margin-top: 32px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  width: 40vw;
+  margin: auto;
+  background-color: #f6f9fa;
+  grid-gap: 16px;
+  border-radius: 16px;
 `;
 
+const BoardBox = styled.div`
+  padding: 32px;
+`;
 
+const DaysBox = styled.div`
+  padding: 32px;
+  grid-column: 1/8;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+
+  grid-gap: 16px;
+`;
 
 const DaysOfWeek = styled.div`
-text-align: center; 
-text-transform: capitalize;
-
-
+  text-align: center;
+  text-transform: capitalize;
+  border-radius: 8px;
 `;
 
+const Day = styled.div`
+  text-align: center;
+  text-transform: capitalize;
+  aspect-ratio: 1 / 1;
+  padding: 4px;
+  background-color: white;
+  border-radius: 8px;
+`;
+
+const EmptyDay = styled.div`
+  text-align: center;
+  text-transform: capitalize;
+  aspect-ratio: 1 / 1;
+  padding: 4px;
+  background-color: white;
+  opacity: 0.5;
+  border-radius: 8px;
+`;
 
 const MonthTitle = styled.div`
+  background-color: #ef4136;
+  grid-column: 1/8;
+  text-align: center;
+  color: white;
+  font-size: 32px;
+  padding: 24px;
 
-    background-color: #EF4136;
-    grid-column: 1/8;
-    text-align: center; 
-    color: white;
-    font-size: 32px;
-    padding: 16px;
-    margin-bottom: 16px;
-
+  border-radius: 16px 16px 0px 0px;
 `;
 
+const dt = new Date();
+const day = dt.getDate();
+const month = dt.getMonth();
+const year = dt.getFullYear();
 
+const firstDayOfMonth = new Date(year, month, 1);
 
+const daysInMonth = new Date(year, month + 1, 0).getDate();
 
-function CalendarDay() {
+const dateString = firstDayOfMonth.toLocaleDateString("en-us", {
+  weekday: "long",
+  year: "numeric",
+  month: "numeric",
+  day: "numeric",
+});
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const paddingDays = weekdays.indexOf(dateString.split(", ")[0]);
+console.log(paddingDays);
 
-}
+console.log("first day of month", firstDayOfMonth);
 
-let daysOfWeekArray = ['domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sabado']
+console.log("Days in month", daysInMonth);
 
-let CalendarGenericArray = () =>{
-    
-}
+//Model of our callendar json
 
+let calendarObj = [
+  {
+    isValideDay: false,
+    day: 1,
+    month: 1,
+    year: 1,
+    date: null,
+    eventsOfDay: null,
+  },
+];
+
+const fillCallendarObj = () => {
+  let numberOfDays = 30;
+  let firstDayIndex = 4;
+  let j = 1;
+  const ourData = [];
+  for (let i = 0; i < numberOfDays + firstDayIndex; i++) {
+    if (i < firstDayIndex) {
+      ourData.push({
+        isValideDay: false,
+      });
+    } else {
+      ourData.push({
+        isValideDay: true,
+        day: j,
+        month: 1,
+        year: 1,
+        date: null,
+        eventsOfDay: [],
+      });
+      j++;
+    }
+  }
+  return ourData;
+};
+
+function CalendarDay() {}
+
+let daysOfWeekArray = [
+  "domingo",
+  "segunda",
+  "terça",
+  "quarta",
+  "quinta",
+  "sexta",
+  "sabado",
+];
+
+let CalendarGenericArray = () => {};
 
 function CalendarBoard() {
-    return (
-        <Board>
-            <MonthTitle>
-               Month
-            </MonthTitle>
-
-            {daysOfWeekArray.map((item, index) => (
-                <DaysOfWeek>{item}</DaysOfWeek>
-            ))}
-
-        </Board>
-    );
+  return (
+    <BoardBox>
+      {" "}
+      <Board>
+        <MonthTitle>Month</MonthTitle>
+        <DaysBox>
+          {" "}
+          {daysOfWeekArray.map((item, index) => (
+            <DaysOfWeek>{item}</DaysOfWeek>
+          ))}
+          {fillCallendarObj().map((item, index) =>
+            item.isValideDay ? <Day>{item.day}</Day> : <EmptyDay> </EmptyDay>
+          )}
+        </DaysBox>
+      </Board>
+    </BoardBox>
+  );
 }
 
 export default CalendarBoard;
