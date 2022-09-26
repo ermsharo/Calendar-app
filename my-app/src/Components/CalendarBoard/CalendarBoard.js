5c047757c1888275cc4349e00b7cb8f969f556c1
emilio <ermsharo@gmail.com>
2022-09-26 17:38:01 -0300

    component in live

--- my-app/src/Components/CalendarBoard/CalendarBoard.js
+++ my-app/src/Components/CalendarBoard/CalendarBoard.js
@@ -3,26 +3,59 @@ import styled from 'styled-components'
 const Board = styled.div`
 display: grid;
 grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-
+width: 60vw;
+padding: 32px;
+margin: auto;
+background-color: #F6F9FA;
+margin-top: 32px;
 `;
+
+
 
 const DaysOfWeek = styled.div`
+text-align: center; 
+text-transform: capitalize;
+
+
+`;
+
+
+const MonthTitle = styled.div`
+
+    background-color: #EF4136;
+    grid-column: 1/8;
+    text-align: center; 
+    color: white;
+    font-size: 32px;
+    padding: 16px;
+    margin-bottom: 16px;
 
 `;
 
 
 
 
-function CalendarDay(){
+function CalendarDay() {
 
 }
 
-let daysOfWeekArray = ['domingo','segunda','terça', 'quarta', 'quinta', 'sexta', 'sabado']
+let daysOfWeekArray = ['domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sabado']
+
+let CalendarGenericArray = () =>{
+    
+}
+
 
 function CalendarBoard() {
     return (
         <Board>
-<DaysOfWeek></DaysOfWeek>
+            <MonthTitle>
+               Month
+            </MonthTitle>
+
+            {daysOfWeekArray.map((item, index) => (
+                <DaysOfWeek>{item}</DaysOfWeek>
+            ))}
 
         </Board>
     );
